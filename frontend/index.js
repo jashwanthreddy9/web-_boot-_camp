7b9deb4d735708fc2efcfa1fda7ee5f0450e210b
jashwanthreddy9 <jashwanthreddy9518@gmail.com>
2026-08-14 10:47:05 +0530

    api setup completed

--- frontend/index.js
+++ frontend/index.js
@@ -21,3 +21,23 @@ function toggleUser(){
     userGender.innerHTML=users[curId].gender;
     userImage.src=users[curId].image;
 }
+function randomUser(){
+    fetch("https://randomuser.me/api")
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+              var userName=document.getElementById("user-name");
+              var userGender=document.getElementById("user-gender");
+              var userImage=document.getElementById("user-image");
+              var newUserName=data.results[0].name.first+" "+ data.results[0].name.first;
+              var newUserGender=data.results[0].gender;
+              var newUserImage=data.results[0].picture.large;
+              userName.innerHTML=newUserName;
+              userGender.innerHTML=newUserGender;
+              userImage.src=newUserImage;
+        })
+        .catch(function(err){
+            console.log("Error occured: "+err);
+        })
+}
